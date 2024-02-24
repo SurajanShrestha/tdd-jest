14e9137f359b63ead9ce5f9e0bd190a99c0ff234
Surajan Shrestha <surajanshrestha35@gmail.com>
2024-02-24 10:33:23 +0545

    fix: used act() in loginform test

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,6 +1,7 @@
 import { render, waitFor } from "@testing-library/react";
 import { LoginForm } from ".";
 import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 
 test("testing a basic Formik form", async () => {
   const handleSubmit = jest.fn();
@@ -11,15 +12,19 @@ test("testing a basic Formik form", async () => {
 
   const user = userEvent.setup();
 
-  // Note 💡: We could've also used "screen.getByRole" instead of destructuring "getByRole" from "render".
-  // We would have imported "screen" from "@testing-library/react".
-  await user.type(getByRole("textbox", { name: /first name/i }), "Alan");
-  await user.type(getByRole("textbox", { name: /last name/i }), "Turing");
+  // Note 💡: When react strict mode is on, it gives a warning when running tests that state updates should be wrapped in "act(...)".
+  // Why use act()? 👉 "act()" makes our tests run closer to how React works in the browser.
+  await act(async () => {
+    // Note 💡: We could've also used "screen.getByRole" instead of destructuring "getByRole" from "render".
+    // We would have imported "screen" from "@testing-library/react".
+    await user.type(getByRole("textbox", { name: /first name/i }), "Alan");
+    await user.type(getByRole("textbox", { name: /last name/i }), "Turing");
 
-  await user.click(getByRole("button", { name: /submit/i }));
-  // Note 💡: This also works 👇, but "user.click" simulates a real user's click interaction as closely as possible than "fireEvent.click".
-  // fireEvent.click(getByRole("button", { name: /submit/i }));
-  // Differences 🤯: "user.click" Simulates full user clicks, triggering events and browser-specific behaviors, whereas "fireClick.click" only triggers click event.
+    await user.click(getByRole("button", { name: /submit/i }));
+    // Note 💡: This also works 👇, but "user.click" simulates a real user's click interaction as closely as possible than "fireEvent.click".
+    // fireEvent.click(getByRole("button", { name: /submit/i }));
+    // Differences 🤯: "user.click" Simulates full user clicks, triggering events and browser-specific behaviors, whereas "fireClick.click" only triggers click event.
+  });
 
   await waitFor(() =>
     expect(handleSubmit).toHaveBeenCalledWith({
