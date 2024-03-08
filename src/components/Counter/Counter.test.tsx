43769adda6167c5c67c3842d27bf6d30d164a820
Surajan Shrestha <surajanshrestha35@gmail.com>
2024-03-08 17:27:52 +0545

    chore: new comment added to explain getByTestId

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -4,6 +4,7 @@ import { Counter } from ".";
 it("increment counter", () => {
   // Note 💡: Acc to testing-library/react, it is recommended to use "getByTestId" only after the other queries don't work for your use case.
   // Reason 💡: Because, your actual code now has an additional "data-testid" attribute, whose purpose is only to be used in tests and does not play any role in the component's logic.
+  // Pros ✅: But, they are way better than selecting elements using css selectors because, css selectors can change often when CSS styles change.
   const { getByTestId } = render(<Counter />);
 
   fireEvent.click(getByTestId("count-up"));
